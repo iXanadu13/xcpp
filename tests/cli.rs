0123fdba8327554cf98d06e6b328ee72c96f6542
Xanadu13 <xanadu13@qq.com>
2024-12-20 15:06:10 +0800

    修改cli测试

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,15 +1,18 @@
-use assert_cmd::prelude::*; // Add methods on commands
-use predicates::prelude::*; // Used for writing assertions
-use std::process::Command; // Run programs
+use assert_cmd::prelude::*;
+use log::info;
+use predicates::prelude::*;
+use std::process::Command;
 
 #[test]
-fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+fn invalid_std_argument() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("xcpp")?;
-
-    cmd.arg("foobar").arg("test/file/doesnt/exist");
+    cmd.current_dir("tmp");
+    info!("{}", cmd.get_current_dir().unwrap().display());
+    cmd.arg("new").arg("hello_cpp")
+        .arg("--std=c++18"); // wrong argument
     cmd.assert()
         .failure()
-        .stderr(predicate::str::contains("could not read file"));
+        .stderr(predicate::str::contains("'c++18' isn't a valid value for '--std <std>'"));
 
     Ok(())
 }
